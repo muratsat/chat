6850a6d3dfa6b2dcabe15acaa497e0859d6b50f7
muratsat <51270744+muratsat@users.noreply.github.com>
2022-04-04 12:20:29 +0300

    Create seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,47 @@
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL UNIQUE ,
+    password_hash VARCHAR(100) NOT NULL ,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE room (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL ,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE participants (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    room_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE message (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    room_id INT,
+    text VARCHAR(1000),
+    date DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE friend (
+    user_id INT,
+    friend_id INT
+);
+
+CREATE TABLE authentication_tokens (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    auth_token VARCHAR(255),
+    generated_at DATETIME,
+    expires_ad DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE friend_requests (
+    user_id int,
+    friend_id int
+);
